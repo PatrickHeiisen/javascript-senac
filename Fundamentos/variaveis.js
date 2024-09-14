05584e6b96bd5c72948c0c243ea61573fa5befe4
Patrick Heiisen <patrickguimaraescorreia@gmail.com>
2024-09-13 21:32:37 -0300

    Variaveis

---
+++ Fundamentos/variaveis.js
@@ -0,0 +1,23 @@
+/**
+ * Estudo de Constante e Variavel
+ * @author Patrick G Correia
+ */
+
+console.clear()
+
+//variaveis
+let nome
+let idade
+let vip
+
+nome = "Patrick G Correia"
+console.log(typeof(nome))
+console.log(nome)
+
+idade = 26
+console.log(typeof(idade))
+console.log(idade)
+
+vip = true
+console.log(typeof(vip))
+console.log(vip)
