6a73d914918ea330579a0994d045654c425a5c19
Patrick Heiisen <patrickguimaraescorreia@gmail.com>
2024-09-19 22:13:09 -0300

    Exercicioos

---
+++ exercicios/academia.js
@@ -0,0 +1,29 @@
+/**
+ * Ficha de um aluno de academia
+ * @author Patrick G Correia
+ */
+
+//Variaveis
+let nome
+let idade
+let altura
+let peso
+let vip
+
+console.clear()
+
+// Entrada de Dados
+nome = "Patrick G"
+idade = 25
+peso = 56
+altura = 1.75
+vip = true
+
+// Saida de Dados
+console.log("Ficha do Aluno")
+console.log("______________________________")
+console.log(`Nome: ${nome}`)
+console.log(`idade: ${idade}`)
+console.log(`peso: ${peso}`)
+console.log(`altura: ${altura}`)
+console.log(vip)
