bef5e9c3f4e37d2f45eb8c6b2125ea8714647b0a
PATRICK GUIMARAES CORREIA <patrickguimaraescorreia@gmail.com>
2025-02-04 22:06:29 -0300

    atualizando

--- formulario_ceps/script.js
+++ formulario_ceps/script.js
@@ -13,12 +13,58 @@ function buscarEndereco() {
         .catch(error => console.error('Erro ao buscar o endereço:', error))
 }
 
-document.getElementById('cadastro').addEventListener('submit', function (event) {
-    event.preventDefault()
+function validarCPF(cpf) {
+    cpf = cpf.replace(/[^\d]+/g, '')
 
-    alert('Cadastro realizado com sucesso!')
-    alert('Dados enviados com sucesso.')
+    if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) {
+        return false
+    }
 
-    this.reset();
-});
+    let soma = 0
+    let resto
 
+    for (let i = 0; i < 9; i++) {
+        soma += parseInt(cpf.charAt(i)) * (10 - i)
+    }
+    resto = soma % 11
+    if (resto < 2) {
+        resto = 0
+    } else {
+        resto = 11 - resto
+    }
+    if (parseInt(cpf.charAt(9)) !== resto) {
+        return false
+    }
+
+    soma = 0
+
+    for (let i = 0; i < 10; i++) {
+        soma += parseInt(cpf.charAt(i)) * (11 - i)
+    }
+    resto = soma % 11
+    if (resto < 2) {
+        resto = 0
+    } else {
+        resto = 11 - resto
+    }
+    if (parseInt(cpf.charAt(10)) !== resto) {
+        return false
+    }
+
+    return true
+}
+
+function validarFormulario(event) {
+    const cpf = document.getElementById('cpf').value
+    
+    if (!validarCPF(cpf)) {
+        alert('CPF inválido. Por favor, insira um CPF válido.')
+        return false
+    }else{
+        alert('Cadastro realizado com sucesso!')
+        return true
+    }
+}
+
+// Adiciona o ouvinte de evento no formulário
+document.getElementById('cadastro').addEventListener('submit', validarFormulario)
